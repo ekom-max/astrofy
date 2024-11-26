90324e46a902ce68f0225208dafbd65b5b349bf7
ekom-max <etokjoyce17@gmail.com>
2024-11-26 14:12:39 +0100

    Update config.ts
    meta description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Joyce Etok| Digital Marketing specialist';
-export const SITE_DESCRIPTION = 'An enthusiastic digital marketing guru,expert in SEO,social media and content marketing.';
+export const SITE_DESCRIPTION = 'A Digital marketing company skilled in SEO,social media and content marketing.executing data driven strategies for business growth.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
